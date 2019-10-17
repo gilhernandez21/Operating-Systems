dbe3c0af68c9ef610d1929b094ec6751ed085de8
herbishere <hrbrtdiaz@gmail.com>
2019-10-17 16:16:10 -0500

    implemented room connections

--- Programs/Program2/diazh.buildrooms.c
+++ Programs/Program2/diazh.buildrooms.c
@@ -10,16 +10,19 @@
 
 struct Room
 {
+    int id;                         // ID OF THE ROOM
     char name[8];                   // NAME OF THE ROOM
     char type[12];                  // TYPE OF ROOM
     int numConnections;             // NUMBER OF CONNECTIONS TO ROOM
-    struct room* connections[6];    // THE CONNECTED ROOMS
+    struct Room* connections[6];    // THE CONNECTED ROOMS
 };
 
 int makeDirectory(char* directoryName);
 int _generateRoomFile(char* directory, char* file, char* input);
 int generateEndIndex(int start, int lastIndex);
+void initializeRoomType(struct Room* room, int index, int startIndex, int endIndex);
 void printRoom(struct Room room);
+char* roomOoutput(struct Room room);
 
 int main()
 {
@@ -39,26 +42,27 @@ int main()
 
     // Allocate Memory for All the Rooms
     struct Room* rooms = malloc(TOTAL_ROOMS * sizeof(struct Room));
-    // Initialize Number of Connections
+    // Initialize ID and Number of Connections
     int count;
     for (count = 0; count < TOTAL_ROOMS; count++)
     {
+        rooms[count].id = count;
         rooms[count].numConnections = 0;
     }
 
     // HARD CODE ROOM NAMES
     char roomNames[TOTAL_ROOM_NAMES][MAX_ROOM_NAME_CHARS];
     memset(roomNames, '\0', TOTAL_ROOM_NAMES * MAX_ROOM_NAME_CHARS * sizeof(char));
-    strcpy(roomNames[0], "A");  // Room 1
-    strcpy(roomNames[1], "B");  // Room 2
-    strcpy(roomNames[2], "C");  // Room 3
-    strcpy(roomNames[3], "D");  // Room 4
-    strcpy(roomNames[4], "E");  // Room 5
-    strcpy(roomNames[5], "F");  // Room 6
-    strcpy(roomNames[6], "G");  // Room 7
-    strcpy(roomNames[7], "H");  // Room 8
-    strcpy(roomNames[8], "I");  // Room 9
-    strcpy(roomNames[9], "J");  // Room 10
+    strcpy(roomNames[0], "Alley");      // Room 1
+    strcpy(roomNames[1], "Bar");        // Room 2
+    strcpy(roomNames[2], "Church");     // Room 3
+    strcpy(roomNames[3], "Dungeon");    // Room 4
+    strcpy(roomNames[4], "Embassy");    // Room 5
+    strcpy(roomNames[5], "Forest");     // Room 6
+    strcpy(roomNames[6], "Guild");      // Room 7
+    strcpy(roomNames[7], "House");      // Room 8
+    strcpy(roomNames[8], "Imps");       // Room 9
+    strcpy(roomNames[9], "Jail");       // Room 10
 
     srand(time(0));     // Seed Random Generator
 
@@ -91,25 +95,58 @@ int main()
     // Set Room Types
     for (count = 0; count < TOTAL_ROOMS; count++)
     {
-        if (count == startIndex)
+        initializeRoomType(&rooms[count], count, startIndex, endIndex);
+    }
+
+    // Create Connections
+    for (count = 0; count < TOTAL_ROOMS; count++)
+    {
+        struct Room* room1 = &rooms[count];
+        int numConnections1 = room1->numConnections;
+        while (numConnections1 < MIN_CONNECTIONS)
         {
-            strcpy(rooms[count].type, "START_ROOM");
-        }
-        else if (count == endIndex)
-        {
-            strcpy(rooms[count].type, "END_ROOM");
-        }
-        else
-        {
-            strcpy(rooms[count].type, "MID_ROOM");
+            int randRoom = (rand() % TOTAL_ROOMS);  // pick random room
+
+            // Make sure random room is not itself
+            if (randRoom != count)
+            {
+                // struct Room* room1 = &rooms[count];
+                // int numConnections1 = room1->numConnections;
+
+                // Check to see if random room is not already connected
+                int isValid = 1;
+                int index;
+                for (index = 0; index < numConnections1; index++)
+                {
+                    struct Room* tempRoom = room1->connections[index];
+                    if (randRoom == tempRoom->id)
+                    {
+                        isValid = 0;
+                        break;
+                    }
+                }
+                
+                // Connect random room
+                if (isValid == 1)
+                {
+                    struct Room* room2 = &rooms[randRoom];
+                    int numConnections2 = room2->numConnections;
+
+                    room1->connections[numConnections1] = room2;
+                    room1->numConnections += 1;
+                    room2->connections[numConnections2] = room1;
+                    room2->numConnections += 1;
+
+                    numConnections1 = room1->numConnections;
+                }
+            }
         }
     }
 
     // DEBUGGING: View Room Information
     for (count = 0; count < TOTAL_ROOMS; count++)
-    {   
-        // View Room Names
-        printf("==ROOM %d:==\n", count + 1);
+    {
+        printf("==ROOM %d:==\n", rooms[count].id + 1);
         printRoom(rooms[count]);
     }
 
@@ -169,9 +206,38 @@ int generateEndIndex(int start, int lastIndex)
     return end;
 }
 
+void initializeRoomType(struct Room* room, int index, int startIndex, int endIndex)
+{
+    if (index == startIndex)
+    {
+        strcpy(room->type, "START_ROOM");
+    }
+    else if (index == endIndex)
+    {
+        strcpy(room->type, "END_ROOM");
+    }
+    else
+    {
+        strcpy(room->type, "MID_ROOM");
+    }
+}
+
+
 void printRoom(struct Room room)
 {
     printf("ROOM NAME: %s\n", room.name);
-    printf("NUM CONNECTIONS: %d\n", room.numConnections);
+    // printf("NUM CONNECTIONS: %d\n", room.numConnections);   // DEBUGGING
+    int count;
+    for (count = 0; count < room.numConnections; count++)
+    {
+        struct Room* tempRoom = room.connections[count];
+        char* roomName = tempRoom->name;
+        printf("CONNECTION %d: %s\n", count + 1, roomName);
+    }
     printf("ROOM TYPE: %s\n", room.type);
+}
+
+char* roomOutput(struct Room room)
+{
+    printf("Hello world!");
 }
