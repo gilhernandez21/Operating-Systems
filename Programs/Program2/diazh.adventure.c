156ed9d1fde8976a030c1ab247ba6bd5f56231d8
herbishere <hrbrtdiaz@gmail.com>
2019-10-21 15:30:09 -0500

    getNewestDirectory() closes directory

--- Programs/Program2/diazh.adventure.c
+++ Programs/Program2/diazh.adventure.c
@@ -6,7 +6,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void getNewestDirectory(char* directoryPrefix, char* directoryName);
+int getNewestDirectory(char* directoryPrefix, char* directoryName);
 
 int main()
 {
@@ -21,58 +21,64 @@ int main()
     return 0;
 }
 
-void getNewestDirectory(char* directoryPrefix, char* directoryName)
+int getNewestDirectory(char* directoryPrefix, char* directoryName)
 {
-    DIR* startDirectory = opendir(".");     // Holds the starting directory
-    struct dirent* subdirectory;            // Holds the subdirectory
-    struct stat attributes;                 // Holds the stats for the newest subdirectory
-    int newestSubTime = -1;                 // Holds the newest subdirectory time
+    int exitStatus = 0;                     // The Exit Status of Function
+    DIR* startDirectory = opendir(".");     // Starting directory
+    struct dirent* subdirectory;            // Subdirectory
+    struct stat attributes;                 // Stats for the newest subdirectory
+    int newestSubTime = -1;                 // Newest subdirectory time
 
     // Check if directory could be opened.
     if (startDirectory > 0)
     {
-        int validDirFound = 0;
+        int validDirFound = 0;  // By default, there is no valid directory.
 
+        // Go through all subdirectories
         while((subdirectory = readdir(startDirectory)) != NULL)
         {
             // Get prefix if it is present in subdirectory name
             char* prefixPresent = strstr(subdirectory->d_name, directoryPrefix);
             // If prefix is present
-            if (prefixPresent != NULL)
+            if (prefixPresent)
             {
-                validDirFound = 1;
+                validDirFound = 1;  // Confirm Valid Directory was found
 
                 char* currentSubName = subdirectory->d_name;    // the current subdirectory name
-
-                // Store subdirectory attributes
-                stat(currentSubName, &attributes);
-
+                stat(currentSubName, &attributes);              // Store subdirectory attributes
                 int currentSubTime = (int)attributes.st_mtime;  // the current subdirectory time
 
                 // Check if current subdirectory is newer than previous newest subdirectory
                 if (newestSubTime < currentSubTime)
                 {
-                    // Set Newest Directory Time
-                    newestSubTime = currentSubTime;
-                    // Reset Newest Directory Name
-                    memset(directoryName, '\0', sizeof(directoryName));
-                    // Save Newest Directory Name
-                    strcpy(directoryName, currentSubName);
+                    newestSubTime = currentSubTime;                     // Set Newest Directory Time
+                    memset(directoryName, '\0', sizeof(directoryName)); // Reset Newest Directory Name
+                    strcpy(directoryName, currentSubName);              // Save Newest Directory Name
                 }
             }
         }
 
+        // Print Error if no valid directory was found.
         if(!validDirFound)
         {
             fprintf(stderr, "ERROR: Failed to Locate Valid Directories.\n");
             fprintf(stderr, "Run Buildrooms Program to Create '%s' Directories.", directoryPrefix);
-            exit(2);
+            exitStatus = 2;
         }
     }
     // Print Error if Start Directory Failed to Open.
     else
     {
         fprintf(stderr, "ERROR: Failed to Open Current Directory.");
-        exit(1);
+        exitStatus = 1;
     }
+
+    closedir(startDirectory);   // Close the Opened Directory
+
+    if(exitStatus != 0)
+    {
+        exit(exitStatus);
+    }
+    return exitStatus;
 }
+
