77857417ae675b0abe10a45321e12b6745eee508
herbishere <hrbrtdiaz@gmail.com>
2019-11-07 14:36:42 -0600

    combined command and arguments together

--- Programs/Program3/smallsh.c
+++ Programs/Program3/smallsh.c
@@ -3,34 +3,35 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 #define SMALLSH_MAX_ARGS 512
 
 void writePrompt();
 void getInput(char** inputPtr, size_t* bufferPtr);
-void inputToComArgs(char* input, char** commandPtr, char** arguments);
+void tokenizeInput(char* input, char** arguments);
 void resetArguments(char** arguments);
 void cleanInput(char** inputPtr);
 void smallsh_cd(char** arguments);
 void smallsh_status(int exitStatus);
 void smallsh_exit(char* input);
 
-int checkInput(char* input, char* command, char** arguments, int* exitStatus)
+int checkInput(char* input, char** arguments, int* exitStatus)
 {
-    if (command != NULL && command[0] != '#')   // Allow Blank Lines and Comments
+    if (arguments[0] != NULL && arguments[0][0] != '#')   // Allow Blank Lines and Comments
     {
         // Built-in Command: 'cd'
-        if (!strcmp(command, "cd"))
+        if (!strcmp(arguments[0], "cd"))
         {
             smallsh_cd(arguments);
         }
         // Built-in Command: 'status'
-        else if (!strcmp(command, "status"))
+        else if (!strcmp(arguments[0], "status"))
         {
             smallsh_status(*exitStatus);
         }
         // Built-in Command: 'exit'
-        else if (!strcmp(command, "exit"))
+        else if (!strcmp(arguments[0], "exit"))
         {
             smallsh_exit(input);
         }
@@ -49,15 +50,14 @@ int main()
     int exitStatus = 0;
     size_t bufferSize = 0;
     char* input = NULL;                 // User Input from stdin
-    char* command;                      // User Command
     char* arguments[SMALLSH_MAX_ARGS];  // Arguments for User Command
 
     do
     {
         writePrompt();
         getInput(&input, &bufferSize);              // Get Input and Allocate Memory
-        inputToComArgs(input, &command, arguments); // Separate Command and Arguments from Input
-        checkInput(input, command, arguments, &exitStatus);
+        tokenizeInput(input, arguments);            // Tokenize Input and Put into Arguments Array
+        checkInput(input, arguments, &exitStatus);
 
         cleanInput(&input);                         // Deallocate Memory for Input
         resetArguments(arguments);                  // Reset all arguments to NULL
@@ -78,14 +78,15 @@ void getInput(char** inputPtr, size_t* bufferPtr)
     (*inputPtr)[numChars - 1] = '\0';
 }
 
-void inputToComArgs(char* input, char** commandPtr, char** arguments)
+void tokenizeInput(char* input, char** arguments)
 {
-    *commandPtr = strtok(input, " ");
+    char* charPtr = input;
 
     int counter = 0;
-    while ((arguments[counter] = strtok(NULL, " ")) != NULL)
+    while ((arguments[counter] = strtok(charPtr, " ")) != NULL)
     {
         counter++;
+        charPtr = NULL;
     }
 }
 
@@ -99,12 +100,18 @@ void resetArguments(char** arguments)
     }
 }
 
+void cleanInput(char** inputPtr)
+{
+    free(*inputPtr);
+    (*inputPtr) = NULL;
+}
+
 void smallsh_cd(char** arguments)
 {
     // char directory[100]                      //DEBUGGING
     // printf("%s\n", getcwd(directory, 100));  //DEBUGGING
     // No Arguments
-    if (arguments[0] == NULL)
+    if (arguments[1] == NULL)
     {
         // Change Directory to HOME environment variable
         chdir(getenv("HOME"));
@@ -113,15 +120,9 @@ void smallsh_cd(char** arguments)
     else
     {
         // Change Directory to Argument.
-        chdir(arguments[0]);
+        chdir(arguments[1]);
     }
     // printf("%s\n", getcwd(directory, 100));  //DEBUGGING
-}
-
-void cleanInput(char** inputPtr)
-{
-    free(*inputPtr);
-    (*inputPtr) = NULL;
 }
 
 void smallsh_status(int exitStatus)
