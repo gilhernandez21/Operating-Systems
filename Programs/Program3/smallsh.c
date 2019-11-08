cea1b2ce391b01bb34238a57b0cc00d78ac63536
herbishere <hrbrtdiaz@gmail.com>
2019-11-08 12:39:58 -0600

    added func that checks command for redirection

--- Programs/Program3/smallsh.c
+++ Programs/Program3/smallsh.c
@@ -4,45 +4,88 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #define SMALLSH_MAX_ARGS 512
 
 void writePrompt();
 void getInput(char** inputPtr, size_t* bufferPtr);
 void tokenizeInput(char* input, char** arguments);
+int checkInput(char* input, char** arguments, int* exitStatus);
 void resetArguments(char** arguments);
 void cleanInput(char** inputPtr);
 void smallsh_cd(char** arguments);
 void smallsh_status(int exitStatus);
 void smallsh_exit(char* input);
-
-int checkInput(char* input, char** arguments, int* exitStatus)
+int checkRedirect(char** arguments)
 {
-    if (arguments[0] != NULL && arguments[0][0] != '#')   // Allow Blank Lines and Comments
+    int index = 0;
+    while (arguments[index] != NULL)
     {
-        // Built-in Command: 'cd'
-        if (!strcmp(arguments[0], "cd"))
+        // If a redirection character is found, return index
+        if(!strcmp(arguments[index], ">") || !strcmp(arguments[index], "<"))
         {
-            smallsh_cd(arguments);
+            return index;
         }
-        // Built-in Command: 'status'
-        else if (!strcmp(arguments[0], "status"))
-        {
-            smallsh_status(*exitStatus);
-        }
-        // Built-in Command: 'exit'
-        else if (!strcmp(arguments[0], "exit"))
-        {
-            smallsh_exit(input);
-        }
-        else
-        {
-            printf("==TODO: PERFORM 'exec' FUNCTION==\n");
-        }
-        
+        index++;
     }
+    // If a redirection character was not found, return -1
+    return -1;
+}
 
-    return 1;
+int executeCmd(char** arguments)
+{
+    if (execvp(*arguments, arguments) < 0)
+    {
+        perror("");
+        return 2;
+    }
+    return 0;
+}
+
+int smallsh_exec(char** arguments)
+{
+    pid_t spawnPid = -3;
+    int childExitMethod = -3;
+
+    // Fork a Child
+    spawnPid = fork();
+    switch (spawnPid)
+    {
+        // Send Error if Fork Failed.
+        case -1:
+        {   perror("fork() failed\n");
+            exit(1);
+            break;
+        }
+        // Execute Command If Child
+        case 0:
+        {
+            // Check For Redirection
+            int redirectIndex;
+            if((redirectIndex = checkRedirect(arguments)) > 0)
+            {
+                // TODO: Setup Redirect
+                // TODO: Perform Action
+
+            }
+            // Otherwise Just Perform the Command
+            else
+            {
+                executeCmd(arguments);   
+            }
+            // Run Exec On Command
+            exit(2);
+            break;
+        }
+        // Let Parent Process Continue Running
+        default:
+        {
+            pid_t actualPid = waitpid(spawnPid, &childExitMethod, 0);
+            return 0;
+            break;
+        }
+    }
 }
 
 int main()
@@ -88,6 +131,35 @@ void tokenizeInput(char* input, char** arguments)
         counter++;
         charPtr = NULL;
     }
+}
+
+int checkInput(char* input, char** arguments, int* exitStatus)
+{
+    if (arguments[0] != NULL && arguments[0][0] != '#')   // Allow Blank Lines and Comments
+    {
+        // Built-in Command: 'cd'
+        if (!strcmp(arguments[0], "cd"))
+        {
+            smallsh_cd(arguments);
+        }
+        // Built-in Command: 'status'
+        else if (!strcmp(arguments[0], "status"))
+        {
+            smallsh_status(*exitStatus);
+        }
+        // Built-in Command: 'exit'
+        else if (!strcmp(arguments[0], "exit"))
+        {
+            smallsh_exit(input);
+        }
+        else
+        {
+            smallsh_exec(arguments);
+        }
+        
+    }
+
+    return 1;
 }
 
 void resetArguments(char** arguments)
