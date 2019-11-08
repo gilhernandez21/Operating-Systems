8987b9554dc0adfc46c30eebc8d1e6cac42fd0a7
herbishere <hrbrtdiaz@gmail.com>
2019-11-08 15:29:32 -0600

    added comments and prototypes

--- Programs/Program3/smallsh.c
+++ Programs/Program3/smallsh.c
@@ -9,130 +9,30 @@
 
 #define SMALLSH_MAX_ARGS 512
 
+/* PROTOTYPES **/
+// Display Prompt
 void writePrompt();
+// Get and Translate User Input
 void getInput(char** inputPtr, size_t* bufferPtr);
 void tokenizeInput(char* input, char** arguments);
+// Check and Process Commands
 int checkInput(char* input, char** arguments, int* exitStatus);
-void resetArguments(char** arguments);
-void cleanInput(char** inputPtr);
-void smallsh_cd(char** arguments);
-void smallsh_status(int exitStatus);
-void smallsh_exit(char* input);
+// Redirection Functions
 int _findString(char** array, char* string);
 int checkRedirectOut(char** arguments);
 int checkRedirectIn(char** arguments);
-void redirectInSetup(int index, int* source, char** arguments)
-{
-    if (index > 0)
-    {
-        char* inputFileName = arguments[index + 1];
-
-        // open source file
-        *source = open(inputFileName, O_RDONLY);
-        if (*source == -1)
-        {
-            perror("cannot open source for input\n");
-            exit(1);
-        }
-        fcntl(*source, F_SETFD, FD_CLOEXEC);     // Close file if it is executed.
-        // redirect stdin to source
-        int result = dup2(*source, 0);
-        if (result == -1)
-        {
-            perror("dup2(source, 0) has failed\n");
-            exit(1);
-        }
-
-        // Setup Argument for Execution if Redirection Successful
-        if (*source != -1 && result != -1)
-        {
-            arguments[index] = '\0';
-        }
-    }
-}
-void redirectOutSetup(int index, int* target, char** arguments)
-{
-    if (index > 0)
-    {
-        char* outputFileName = arguments[index + 1];
-
-        // open target file
-        *target = open(outputFileName, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-        if (*target == -1)
-        {
-            perror("cannot open target for output\n");
-            exit(2);
-        }
-        fcntl(*target, F_SETFD, FD_CLOEXEC);     // Close file if it is executed.
-        // redirect stdin to source
-        int result = dup2(*target, 1);
-        if (result == -1)
-        {
-            perror("dup2(target, 1) has failed\n");
-            exit(2);
-        }
-        // Setup Argument for Execution if Redirection Successful
-        if (*target != -1 && result != -1)
-        {
-            arguments[index] = '\0';
-        }
-    }
-}
-int executeCmd(char** arguments)
-{
-    if (execvp(*arguments, arguments) < 0)
-    {
-        perror("");
-        return 2;
-    }
-    return 0;
-}
-
-int smallsh_exec(char** arguments)
-{
-    pid_t spawnPid = -3;
-    int childExitMethod = -3;
-
-    // Fork a Child
-    spawnPid = fork();
-    switch (spawnPid)
-    {
-        // Send Error if Fork Failed.
-        case -1:
-        {   perror("fork() failed\n");
-            exit(1);
-            break;
-        }
-        // Execute Command If Child
-        case 0:
-        {
-            int source = -3,    // The Source File Descriptor
-                target = -3,    // The Target File Descriptor
-                redirectInIndex = checkRedirectIn(arguments),   // Index of "<" character
-                redirectOutIndex = checkRedirectOut(arguments); // Index of ">" character
-
-            // Setup Redirection if Redirection Detected
-            if (redirectInIndex > 0 || redirectOutIndex > 0)
-            {
-                // If Valid, Setup stdin
-                redirectInSetup(redirectInIndex, &source, arguments);
-                // If Valid, Setup stdout
-                redirectOutSetup(redirectOutIndex, &target, arguments);
-            }
-            // Execute the Command
-            executeCmd(arguments);
-            exit(3);
-            break;
-        }
-        // Let Parent Process Continue Running
-        default:
-        {
-            pid_t actualPid = waitpid(spawnPid, &childExitMethod, 0);
-            return 0;
-            break;
-        }
-    }
-}
+void redirectInSetup(int index, int* source, char** arguments);
+void redirectOutSetup(int index, int* target, char** arguments);
+// Execution Functions
+int executeCmd(char** arguments);
+int smallsh_exec(char** arguments);
+// Built-In Functions
+void smallsh_cd(char** arguments);
+void smallsh_status(int exitStatus);
+void smallsh_exit(char* input);
+// Cleanup Functions
+void resetArguments(char** arguments);
+void cleanInput(char** inputPtr);
 
 int main()
 {
@@ -198,6 +98,7 @@ int checkInput(char* input, char** arguments, int* exitStatus)
         {
             smallsh_exit(input);
         }
+        // Execute Command
         else
         {
             smallsh_exec(arguments);
@@ -281,4 +182,119 @@ int checkRedirectOut(char** arguments)
 int checkRedirectIn(char** arguments)
 {
     return _findString(arguments, "<");
+}
+
+void redirectInSetup(int index, int* source, char** arguments)
+{
+    if (index > 0)
+    {
+        char* inputFileName = arguments[index + 1];
+
+        // open source file
+        *source = open(inputFileName, O_RDONLY);
+        if (*source == -1)
+        {
+            perror("cannot open source for input\n");
+            exit(1);
+        }
+        fcntl(*source, F_SETFD, FD_CLOEXEC);     // Close file if it is executed.
+        // redirect stdin to source
+        int result = dup2(*source, 0);
+        if (result == -1)
+        {
+            perror("dup2(source, 0) has failed\n");
+            exit(1);
+        }
+
+        // Setup Argument for Execution if Redirection Successful
+        if (*source != -1 && result != -1)
+        {
+            arguments[index] = '\0';
+        }
+    }
+}
+
+void redirectOutSetup(int index, int* target, char** arguments)
+{
+    if (index > 0)
+    {
+        char* outputFileName = arguments[index + 1];
+
+        // open target file
+        *target = open(outputFileName, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (*target == -1)
+        {
+            perror("cannot open target for output\n");
+            exit(2);
+        }
+        fcntl(*target, F_SETFD, FD_CLOEXEC);     // Close file if it is executed.
+        // redirect stdin to source
+        int result = dup2(*target, 1);
+        if (result == -1)
+        {
+            perror("dup2(target, 1) has failed\n");
+            exit(2);
+        }
+        // Setup Argument for Execution if Redirection Successful
+        if (*target != -1 && result != -1)
+        {
+            arguments[index] = '\0';
+        }
+    }
+}
+
+int executeCmd(char** arguments)
+{
+    if (execvp(*arguments, arguments) < 0)
+    {
+        perror("");
+        return 2;
+    }
+    return 0;
+}
+
+int smallsh_exec(char** arguments)
+{
+    pid_t spawnPid = -3;
+    int childExitMethod = -3;
+
+    // Fork a Child
+    spawnPid = fork();
+    switch (spawnPid)
+    {
+        // Send Error if Fork Failed.
+        case -1:
+        {   perror("fork() failed\n");
+            exit(1);
+            break;
+        }
+        // Execute Command If Child
+        case 0:
+        {
+            int source = -3,    // The Source File Descriptor
+                target = -3,    // The Target File Descriptor
+                redirectInIndex = checkRedirectIn(arguments),   // Index of "<" character
+                redirectOutIndex = checkRedirectOut(arguments); // Index of ">" character
+
+            // Setup Redirection if Redirection Detected
+            if (redirectInIndex > 0 || redirectOutIndex > 0)
+            {
+                // If Valid, Setup stdin
+                redirectInSetup(redirectInIndex, &source, arguments);
+                // If Valid, Setup stdout
+                redirectOutSetup(redirectOutIndex, &target, arguments);
+            }
+            // Execute the Command
+            executeCmd(arguments);
+            exit(3);
+            break;
+        }
+        // Let Parent Process Continue Running
+        default:
+        {
+            pid_t actualPid = waitpid(spawnPid, &childExitMethod, 0);
+            return 0;
+            break;
+        }
+    }
 }
