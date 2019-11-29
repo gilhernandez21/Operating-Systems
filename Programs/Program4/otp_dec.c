78de5e0bf14ebad8ff98c4f1c0d23c77dea39957
herbishere <hrbrtdiaz@gmail.com>
2019-11-29 16:09:27 -0600

    put in starting code

---
+++ Programs/Program4/otp_dec.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/ioctl.h>
+
+#include "otp_helpers.h"
+#define h_addr h_addr_list[0]
+
+// File Validation
+int checkFile(char* fileName);
+void validateFiles(char* plaintext, char* key);
+int sendFile(char* source, char* fileName, char buffer[], char* termString, int socketFD);
+
+int main(int argc, char *argv[])
+{
+	char* clientVerifier = "OTP_DEC";
+	char* terminationString = "$OTP_TERMINATE";
+	char* source = "CLIENT";
+
+	int socketFD, portNumber, charsWritten, charsRead;
+	struct sockaddr_in serverAddress;
+	struct hostent* serverHostInfo;
+	char buffer[OTP_BUFFERSIZE];
+    
+	if (argc < 4) { fprintf(stderr,"USAGE: %s [ciphertext] [key] [port]\n", argv[0]); exit(0); } // Check usage & args
+
+	// Check files for bad characters and proper lengths
+	validateFiles(argv[1], argv[2]);
+
+	// Set up the server address struct
+	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+	portNumber = atoi(argv[3]); // Get the port number, convert to an integer from a string
+	serverAddress.sin_family = AF_INET; // Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber); // Store the port number
+	serverHostInfo = gethostbyname("localhost"); // Convert the machine name into a special form of address
+	if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
+	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
+
+	// Set up the socket
+	socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+	if (socketFD < 0) error("CLIENT: ERROR opening socket");
+	
+	// Connect to server
+	if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
+		error("CLIENT: ERROR connecting");
+
+	// Send verifier to server and get response
+	sendMessage(source, clientVerifier, socketFD);
+	getResponse(source, buffer, socketFD);
+	// If server sends unsuccessful response, print error and exit.
+	if (atoi(buffer) != 200)
+	{
+		fprintf(stderr, "ERROR: Cannot connect to server on port: %d\n", portNumber);
+		exit(2);
+	}
+
+	// Send ciphertext to server
+	sendFile(source, argv[1], buffer, terminationString, socketFD);
+
+	// Send keygen to server
+	sendFile(source, argv[2], buffer, terminationString, socketFD);
+
+	// Get plaintext and print to stdout
+	while(1)
+	{
+		getResponse(source, buffer, socketFD);
+		sendMessage(source, "200", socketFD);
+
+		if (strcmp(buffer, terminationString))
+		{
+			printf("%s", buffer);
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	close(socketFD); // Close the socket
+	return 0;
+}
+
+int checkFile(char* fileName)
+{
+    int character;  // holds the integer value of the character
+    int count = 0;  // holds the number of characters in the file
+
+    // Open the file
+    FILE* fileInput = fopen(fileName, "r");
+    // Check if valid
+    if (fileInput == NULL) { fprintf(stderr,"ERROR failed to open '%s'\n", fileName); exit(1); }
+
+    // Go through the file
+    while ((character = fgetc(fileInput)) != EOF)
+    {
+        // If an invalid character is detected, print error and exit
+        if ((character < 'A' || character > 'Z') && character != ' ' && character != '\n')
+        {
+            fprintf(stderr,"ERROR '%s' contains invalid characters\n", fileName);
+            exit(1);
+        }
+        count++;
+    }
+
+    // Close the file
+    fclose(fileInput);
+
+    return count;
+}
+
+void validateFiles(char* plaintext, char* key)
+{
+    // Check if files are valid and record number of characters
+    int plaintextCount = checkFile(plaintext);
+    int keyCount = checkFile(key);
+
+    // If the key file is shorter than the plaintext, terminate and send error
+    if (keyCount < plaintextCount)
+    {
+        fprintf(stderr, "ERROR key file '%s' shorter than plaintext '%s'\n", key, plaintext);
+        exit(1);
+    }
+}
+
+int sendFile(char* source, char* fileName, char buffer[], char* termString, int socketFD)
+{
+
+	FILE* fileInput = fopen(fileName, "r"); // Open plaintext file
+	memset(buffer, '\0', OTP_BUFFERSIZE); // Clear out the buffer array
+
+	int count = 0;
+
+	char fileBuffer[OTP_BUFFERSIZE];
+	while(fgets(fileBuffer, OTP_BUFFERSIZE, fileInput))
+	{
+		count++;
+		
+		// Send message to server
+		sendMessage(source, fileBuffer, socketFD);
+		// Get return message from server
+		getResponse(source, buffer, socketFD);
+
+		memset(buffer, '\0', OTP_BUFFERSIZE); // Clear out the buffer array
+	}
+	// Send Termination Signal
+	sendMessage(source, termString, socketFD);
+	getResponse(source, buffer, socketFD);
+	// Close File
+	fclose(fileInput);
+}
---
+++ Programs/Program4/otp_dec_d.c
@@ -0,0 +1,234 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+
+#include "otp_helpers.h"
+
+int sendVerificationResult(char buffer[], char* clientVerifier, int establishedConnectionFD);
+int getClientFile(char* source, char buffer[], char* termString, char** fileString, int establishedConnectionFD);
+
+int sendString(char* output, char buffer[], char* terminationString, int fileDescriptor)
+{
+    int bufferIndex = 0;
+	char* source = "SERVER";
+
+    // Loop Through the String, building a packet to send
+    int index;
+    for (index = 0; index < strlen(output); index++)
+    {
+        // If packet reaches 
+        if (bufferIndex == OTP_BUFFERSIZE)
+        {
+            bufferIndex = 0;
+            // printf("Packet: '%s'\n", buffer);
+			sendMessage(source, buffer, fileDescriptor);
+			getResponse(source, buffer, fileDescriptor);
+            memset(buffer, '\0', OTP_BUFFERSIZE);
+        }
+        buffer[bufferIndex] = output[index];
+        bufferIndex++;
+    }
+    // Send the remaining buffer
+    // printf("Packet: '%s'\n", buffer);
+	sendMessage(source, buffer, fileDescriptor);
+	getResponse(source, buffer, fileDescriptor);
+    // Send termination character
+    // printf("%s", terminationString);
+	sendMessage(source, terminationString, fileDescriptor);
+	getResponse(source, buffer, fileDescriptor);
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	char* clientVerifier = "OTP_ENC";
+	char* terminationString = "$OTP_TERMINATE";
+	char* source = "SERVER";
+
+	int listenSocketFD, establishedConnectionFD, portNumber, charsRead;
+	socklen_t sizeOfClientInfo;
+	char buffer[OTP_BUFFERSIZE];
+	struct sockaddr_in serverAddress, clientAddress;
+
+	pid_t backPIDs[OTP_MAX_CONNECTIONS];
+	int index;
+	for (index = 0; index < OTP_MAX_CONNECTIONS; index++)
+	{
+		backPIDs[index] = 1;
+	}
+
+	if (argc < 2) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
+
+	// Set up the address struct for this process (the server)
+	memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+	portNumber = atoi(argv[1]); // Get the port number, convert to an integer from a string
+	serverAddress.sin_family = AF_INET; // Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber); // Store the port number
+	serverAddress.sin_addr.s_addr = INADDR_ANY; // Any address is allowed for connection to this process
+
+	// Set up the socket
+	listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+	if (listenSocketFD < 0) error("ERROR opening socket");
+
+	// Enable the socket to begin listening
+	if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to port
+		error("ERROR on binding");
+	listen(listenSocketFD, OTP_MAX_CONNECTIONS); // Flip the socket on - it can now receive up to 5 connections
+
+	do
+	{
+		// Accept a connection, blocking if one is not available until one connects
+		sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
+		establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+		if (establishedConnectionFD < 0) error("ERROR on accept");
+
+		pid_t spawnPID = -5;
+		int childExitMethod = -5;
+
+		// Create a fork to get the files, encode the message, the send the ciphertext
+		spawnPID = fork();
+
+		switch (spawnPID)
+		{
+			// Catch errors
+			case -1:
+			{
+				error("fork() failed\n");
+				exit(1);
+				break;
+			}
+			// Get the files, encode the message, send the ciphertext
+			case 0:
+			{
+				// Get verifification message from client and send result code back
+				getResponse(source, buffer, establishedConnectionFD);
+				// getFromClient(buffer, establishedConnectionFD);
+				sendVerificationResult(buffer, clientVerifier, establishedConnectionFD);
+
+				// Initialize the OneTimePad files;
+				struct OneTimePad pad;
+				initOTP(&pad);
+
+				// Get Cipher Text
+				getClientFile(source, buffer, terminationString, &pad.ciphertext, establishedConnectionFD);
+
+				// Get Key
+				getClientFile(source, buffer, terminationString, &pad.key, establishedConnectionFD);
+
+				// Encode Text
+				OTP_decode(&pad);
+
+				// Send the cipher text to client
+				sendString(pad.plaintext, buffer, terminationString, establishedConnectionFD);
+				
+				freeOTP(&pad);					// Clear the One Time Pad
+				close(establishedConnectionFD); // Close the existing socket which is connected to the client
+				exit(0);
+				break;
+			}
+			// Save the process, check for any completed processes, and continue recieving requests
+			default:
+			{
+				int savedPID = 0;	// Flag checks to see if background PID was saved
+				// Add Process to Empty PID
+				int index;
+				for (index = 0; index < OTP_MAX_CONNECTIONS; index++)
+				{
+					// Check if Process has Been Completed, if so store background process
+					pid_t actualPID = waitpid(backPIDs[index], &childExitMethod, WNOHANG);
+					// printf("Actual PID: %d, Background PID: %d\n", actualPID, backPIDs[index]); // DEBUGGING
+					if (actualPID)
+					{
+						backPIDs[index] = spawnPID;
+						// printf("Stored PID: %d\n", backPIDs[index]); DEBUGGING
+						savedPID = 1;
+						break;
+					}
+				}
+				// If process ID couldn't be saved, print error and retrieve child
+				if (!savedPID)
+				{
+					fprintf(stderr, "ERROR: Processes exceed max connections, retriveing child...\n");
+					waitpid(spawnPID, &childExitMethod, 0);
+				}
+				break;
+			}
+		}
+
+		// Wait Children
+		int index;
+		for (index = 0; index < OTP_MAX_CONNECTIONS; index++)
+		{
+			// Check if Process has Been Completed
+			pid_t actualPID = waitpid(backPIDs[index], &childExitMethod, WNOHANG);
+		}
+
+	} while(1);
+
+	close(listenSocketFD); // Close the listening socket
+
+	// catch all remaining children
+	int childExitMethod = -5;
+	for (index = 0; index < OTP_MAX_CONNECTIONS; index++)
+	{
+		// Check if Process has Been Completed
+		pid_t actualPID = waitpid(backPIDs[index], &childExitMethod, 0);
+	}
+	return 0; 
+}
+
+int sendVerificationResult(char buffer[], char* clientVerifier, int establishedConnectionFD)
+{
+	int charsRead;
+
+	if (!strcmp(buffer, clientVerifier))
+	{
+		charsRead = send(establishedConnectionFD, "200", 3, 0); // Send success back
+		if (charsRead < 0) error("ERROR writing to socket");
+	}
+	else
+	{
+		charsRead = send(establishedConnectionFD, "403", 3, 0); // Send failure back
+		if (charsRead < 0) error("ERROR writing to socket");
+		close(establishedConnectionFD); // Close the existing socket which is connected to the client
+		return 1;
+	}
+	return 0;
+}
+
+int getClientFile(char* source, char buffer[], char* termString, char** fileString, int establishedConnectionFD)
+{
+	int charsRead;
+
+	// Continuously receieve from client, saving to fileString until termination
+	// string is recieved from the client.
+	while(1)
+	{
+		// Get part of the file
+		getResponse(source, buffer, establishedConnectionFD);
+
+		// Send confirmation that message was recieved to client
+		charsRead = send(establishedConnectionFD, "200", 3, 0); // Send success back
+		if (charsRead < 0) error("ERROR writing to socket");
+
+		// If termination string wasn't recieved, save string
+		if (strcmp(buffer, termString))
+		{
+			appendString(fileString, buffer);
+		}
+		// Otherwise, exit the loop
+		else
+		{
+			appendString(fileString, "\0");
+			break;
+		}
+	}
+
+	return 0;
+}
